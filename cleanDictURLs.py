e55f4ab96740d2220aae57e3df9f58168ab5a72b
J-Spade <j_spade@verizon.net>
2016-01-16 04:20:45 -0500

    utility fix

--- cleanDictURLs.py
+++ cleanDictURLs.py
@@ -1,39 +1,37 @@
 import pickle
 import re
 
+STOPWORD = 'BOGON'
+
 input = open('Markov_Dict.pkl', 'r')
 dictionary = pickle.load(input)
 input.close()
 
 keystochange = []
 
+#       key        come-befores       come-afters
+newdict = { STOPWORD : ( [ (STOPWORD, 1) ], [ (STOPWORD, 1) ] ) }
+
 for key in dictionary:
+	newkey = key
 	firsts = dictionary.get(key)[0]
 	for i in range (0, len(firsts)):
 		if re.match(r'^<https?:\/\/.+\|?.*>$', firsts[i][0]):
 			firsts[i][0] = firsts[i][0][1:-1]
 			if '|' in firsts[i][0]:
 				firsts[i][0] = firsts[i][0].split('|')[1]
-	key[0] = firsts
 	seconds = dictionary.get(key)[1]
 	for i in range (0, len(seconds)):
 		if re.match(r'^<https?:\/\/.+\|?.*>$', seconds[i][0]):
 			seconds[i][0] = seconds[i][0][1:-1]
 			if '|' in seconds[i][0]:
 				seconds[i][0] = seconds[i][0].split('|')[1]
-	key[1] = seconds
 	if re.match(r'^<https?:\/\/.+\|?.*>$', key):
-		keystochange.append(key)
-
-for key in keystochange:
-		newkey = key
 		newkey = newkey[1:-1]
 		if '|' in newkey:
 			newkey = newkey.split('|')[1]
-		dictionary[newkey] = dictionary.get(key)
-		del dictionary[key]
+	newdict[newkey] = (firsts, seconds)
 
 output = open('Markov_Dict.pkl', 'w')
-pickle.dump(self.dictionary, output)
+pickle.dump(newdict, output)
 output.close()
-		
